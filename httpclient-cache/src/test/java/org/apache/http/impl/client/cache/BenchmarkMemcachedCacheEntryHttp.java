e9236457d557530e98e9cb9dff8fdbc33985a84d
Scott Gifford <sggiffor@amazon.com>
2020-01-05 17:11:57 -0500

    Increase runs in BenchmarkMemcachedCacheEntryHttp.

--- httpclient-cache/src/test/java/org/apache/http/impl/client/cache/BenchmarkMemcachedCacheEntryHttp.java
+++ httpclient-cache/src/test/java/org/apache/http/impl/client/cache/BenchmarkMemcachedCacheEntryHttp.java
@@ -148,7 +148,7 @@ public class BenchmarkMemcachedCacheEntryHttp {
         }
 
         final long start = System.nanoTime();
-        final int runs = 5000;
+        final int runs = 50000;
         for(int i=0;i<runs;i++) {
             func.run();
         }
